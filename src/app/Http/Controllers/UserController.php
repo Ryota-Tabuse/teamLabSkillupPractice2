1ff8f1c8f7ea82678650e46bc2af419e1b9581e9
ryota-tabuse <bububutase.0107@gmail.com>
2019-12-01 17:30:19 +0900

    DB接続テスト

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -2,11 +2,15 @@
 namespace APP\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\User;
 
 class UserController extends Controller{
 	public function index()
 	{
-		$name = 'yamada taro';
-		return view('user', ['name' => $name]);
+		$email = substr(str_shuffle('abcdefghijklmnopqrstyvwxyz'), 0, 8) . 'yyy.com';
+		User::insert(['name' => 'yamada taro', 'email' => $email, 'password' => 'xxxxxxxx']);
+
+		$users = User::all();
+		return view('user', ['users' => $users]);
 	}
 }
